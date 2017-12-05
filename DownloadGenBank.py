cc74abb73ee6ab49dfa7891172f1d69799e098c7
Carl Malina <malcarl@chalmers.se>
2017-12-05 18:48:28 +0100

    Added option for downloading multiple files

--- DownloadGenBank.py
+++ DownloadGenBank.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python
 
-### This script downoads GenBank files from NCBI using Entrez from biopython ### 
+### This script downloads GenBank files from NCBI using Entrez from biopython ### 
 
-# Check that the library is installed and that it can be loaded
-try:
-	from Bio import Entrez as entrez
-except:
-	print 'Cannot import Entrez'
+# Import the Entrez module from biopython
+from Bio import Entrez as entrez
 
-with open('testFile.gb','w') as outfile:
-	entrez.email = 'carl_malina@hotmail.com'
-	gbFile = entrez.efetch(db = 'nucleotide', id = 'NC_002333.1', rettype = 'gb')
-	try:
-		outfile.write(gbFile.read())
-	except:
-		print 'Cannot write to file'
+# Import os module to enable OS functionalities
+import os
+
+# Specify the output path for the GenBank files
+output_dir = ('/Users/malcarl/Documents/Courses/python-programming-for-life-science-researchers/'
+			  'sequence_files/Python-programming-for-life-science-researchers/GenBankFiles')
+# Move to directory
+os.chdir(output_dir)
+locusIDs = []
+# Read file containing locusIDs
+count = 0
+with open('fungalMitochondrialLocusIDs.txt','r') as infile:
+	for line in infile:
+		count += 1
+		line = line.strip()
+		ID = line.split('\t')[1]
+		ID = ID.strip()
+		# Select ncbi ID if available
+		if '/' in ID:
+			pos_slash = ID.find('/')
+			new_ID = ID[:pos_slash]
+			locusIDs.append(new_ID)
+		else:
+			locusIDs.append(ID)
+
+# Run Genbank query for all RefSeq IDs:
+count = 0
+num_IDs = len(locusIDs)
+for ID in locusIDs:
+	with open(ID + '.gb','w') as outfile:
+		entrez.email = 'carl_malina@hotmail.com'
+		gbFile = entrez.efetch(db = 'nucleotide', id = ID, rettype = 'gb')
+		try:
+			outfile.write(gbFile.read())
+		except:
+			print 'Cannot write to file'
+			pass
+	count += 1
+	print 'Done with %d of %d IDs' % (count, num_IDs)
+
