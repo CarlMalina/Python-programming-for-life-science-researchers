cde5c19155f60e2235f3e3a470f80e70c013eb18
Carl Malina <malcarl@chalmers.se>
2018-01-15 13:22:57 +0100

    Made input and output path optional

--- Miscellaneous/ConvertGenBank.py
+++ Miscellaneous/ConvertGenBank.py
@@ -7,13 +7,19 @@ import os
 # Import the sequence input/output module from biopython
 from Bio import SeqIO
 
-input_dir = ('/Users/malcarl/Documents/Courses/python-programming-for-life-science-researchers/'
-			 'sequence_files/Python-programming-for-life-science-researchers/Miscellaneous')
-output_dir = ('/Users/malcarl/Documents/Courses/python-programming-for-life-science-researchers/'
-			 'sequence_files/Python-programming-for-life-science-researchers/Miscellaneous')
+input_dir = '' #('/Users/malcarl/Documents/Courses/python-programming-for-life-science-researchers/'
+			 #'sequence_files/Python-programming-for-life-science-researchers/Miscellaneous')
+output_dir = '' #('/Users/malcarl/Documents/Courses/python-programming-for-life-science-researchers/'
+			 #'sequence_files/Python-programming-for-life-science-researchers/Miscellaneous')
+
+# Change to input_dir if specified
+if input_dir != '':
+	os.chdir(input_dir)
+
 # list all GenBank files
+current_directory = os.getcwd()
 GenBank_files = []
-for filename in os.listdir(input_dir):
+for filename in os.listdir(current_directory):
 	if filename.endswith('.gb'):
 		GenBank_files.append(filename)
 
@@ -25,14 +31,16 @@ for filename in GenBank_files:
 	IDs.append(ID)
 
 # Construct FASTA file from GenBank file
-os.chdir(output_dir)
+# change to output_dir if specified
+if output_dir != '':
+	os.chdir(output_dir)
 count = 0
 
 for file in GenBank_files:
 	count += 1
 	print 'Processing file %d of %d' % (count, len(IDs))
 	try:
-		with open(os.path.join(input_dir,file),'r') as input_file, open(IDs[count-1] + '.fna','w') as output_file:
+		with open(os.path.join(current_directory,file),'r') as input_file, open(IDs[count-1] + '.fna','w') as output_file:
 			for seq_record in SeqIO.parse(input_file,"genbank"):
 				print 'Dealing with GenBank ID %s' % seq_record.id
 				output_file.write('>%s %s\n%s' % (seq_record.id,
